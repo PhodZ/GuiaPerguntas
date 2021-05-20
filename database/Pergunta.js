52027717e8021422405543e8757fb8ef19f4557a
PhodZ <84547477+PhodZ@users.noreply.github.com>
2021-05-20 20:13:32 -0300

    Add files via upload

---
+++ database/Pergunta.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const connection = require('./database');
+
+const Pergunta = connection.define('perguntas', {
+    titulo:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    descricao:{
+        type:Sequelize.TEXT,
+        allowNull:false
+    }
+});
+
+Pergunta.sync({force:false}).then(()=>{
+    console.log("Tabela criada!");
+}).catch((err)=>{
+    console.log(err);
+});
+
+module.exports = Pergunta;
